a2dee7fd3da97f555c821bb5fd663238c47c6562
Lexielist <48461313+Lexielist@users.noreply.github.com>
2019-04-10 23:25:11 -0400

    Add files via upload

---
+++ IntermediateEliza.py
@@ -0,0 +1,55 @@
+asking = True
+
+rplist = {"i":"you","me":"you","my":"your","am":"are"}
+
+newAnswer = ""
+
+while asking:
+
+	answer = input("How are you? Please type how you feel or 'Q' to quit: ")
+
+	
+
+	if str.lower(answer) == "good" or str.lower(answer) == "q":
+
+		asking = False
+
+	else:
+
+		answerlist = answer.split()
+
+		for item in answerlist:
+
+			#if i== rplist.keys():
+
+			#if item in rplist:
+
+			#    answer.replace(item, rplist[item])
+
+			if item == "i":
+
+				item = "you"
+
+			if item == "me":
+
+				item = "you"
+
+			if item == "my":
+
+				item = "your"
+
+			if item == "am":
+
+				item = "are"
+
+			# end of if statement
+
+		newAnswer = newAnswer + item + " "
+
+		print(newAnswer)
+
+			
+
+		print("That's not what I'm asking for...please answer \"good or somthing else\"")
+
+print("Thank you")
