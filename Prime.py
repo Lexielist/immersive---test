7457845a119dc56ed8d2a24508212bedc796a140
Lexielist <48461313+Lexielist@users.noreply.github.com>
2019-04-12 17:01:17 -0400

    Add files via upload

---
+++ Prime.py
@@ -0,0 +1,9 @@
+A = input("Please input a number:    ")
+
+print ("1 is not a prime number")
+for number in range (2,A):
+
+	if num%A == 0:
+		print (num," is a prime number)
+	
+	else: print (num,"is not a prime number)
